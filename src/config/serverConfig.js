ed9efb427f322b2284d708e93f8f96761a46faae
futoid <aliekmandal24@gmail.com>
2023-11-15 13:39:45 +0530

    auth api added

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,9 +1,23 @@
+const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 const bcrypt = require("bcrypt");
+
+//Database Connection
+async function connectToDB() {
+  const sequelize = new Sequelize(process.env.DEV_DATABASE_URL);
+
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
 
 module.exports = {
   PORT: process.env.PORT,
   SALT: bcrypt.genSaltSync(10),
   JWT_KEY: process.env.JWT_KEY,
+  dbconnect: connectToDB,
 };
